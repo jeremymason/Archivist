7365d850b8241f70e70cb4836b16837bd250d360
Jeremy Mason <jeremycmason@gmail.com>
2011-08-28 15:11:08 -0400

    add inlines for other participants

--- archivist/web/admin.py
+++ archivist/web/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from models import *
 
+class DigitalFileParticipantInline(admin.TabularInline):
+    model = DigitalFileParticipant
+    extra = 1
+
+class DigitalFileAdmin(admin.ModelAdmin):
+    inlines = (DigitalFileParticipant,)
+
+
+class SeriesParticipantInline(admin.TabularInline):
+    model = SeriesParticipant
+    extra = 1
+
+class SeriesAdmin(admin.ModelAdmin):
+    inlines = (SeriesParticipantInline,)
+
+
+class SourceParticipantInline(admin.TabularInline):
+    model = SourceParticipant
+    extra = 1
+
+class SourceAdmin(admin.ModelAdmin):
+    inlines = (SourceParticipantInline,)
+
+
 class ProgramParticipantInline(admin.TabularInline):
     model = ProgramParticipant
     extra = 1
@@ -11,12 +35,13 @@ class ProgramAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
     list_filter = ('title', 'genre')
 
+
 admin.site.register(Program, ProgramAdmin)
-admin.site.register(DigitalFile)
+admin.site.register(DigitalFile, DigitalFileAdmin)
 admin.site.register(DigitalFileLocation)
 admin.site.register(SourceFormat)
-admin.site.register(Source)
-admin.site.register(Series)
+admin.site.register(Source, SourceAdmin)
+admin.site.register(Series, SeriesAdmin)
 admin.site.register(Rights)
 admin.site.register(Genre)
 admin.site.register(Subject)
