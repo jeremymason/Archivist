535713ecc5616b2571356894f6c32ac9286f2116
Jeremy Mason <jeremycmason@gmail.com>
2011-12-21 21:43:12 -0500

    remove tabs

--- archivist/web/views.py
+++ archivist/web/views.py
@@ -3,50 +3,50 @@ from web.models import Program, Genre, Subject
 from django.http import HttpResponse
 
 def index(request):
-	t = loader.get_template('home.html')
-	programs = Program.objects.all()
-	c = RequestContext(request, {
-		'programs': programs,
-		})
-	return HttpResponse(t.render(c))
+    t = loader.get_template('home.html')
+    programs = Program.objects.all()
+    c = RequestContext(request, {
+        'programs': programs,
+        })
+    return HttpResponse(t.render(c))
 
 
 def genres(request):
-	t = loader.get_template('display.html')
-	items = Genre.objects.all()
-	items = [x.genre for x in items]
-	c = RequestContext(request, {
-		'item_name': "Genres",
-		'items': items,
-		})
-	return HttpResponse(t.render(c))
+    t = loader.get_template('display.html')
+    items = Genre.objects.all()
+    items = [x.genre for x in items]
+    c = RequestContext(request, {
+        'item_name': "Genres",
+        'items': items,
+        })
+    return HttpResponse(t.render(c))
 
 
 def subjects(request):
-	t = loader.get_template('display.html')
-	items = Subject.objects.all()
-	items = [x.subject for x in items]
-	c = RequestContext(request, {
-		'item_name': "Subjects",
-		'items': items,
-		})
-	return HttpResponse(t.render(c))
+    t = loader.get_template('display.html')
+    items = Subject.objects.all()
+    items = [x.subject for x in items]
+    c = RequestContext(request, {
+        'item_name': "Subjects",
+        'items': items,
+        })
+    return HttpResponse(t.render(c))
 
 
 def programs(request):
-	t = loader.get_template('programs.html')
-	programs = Program.objects.all()
-	c = RequestContext(request, {
-		'programs': programs,
-		})
-	return HttpResponse(t.render(c))
+    t = loader.get_template('programs.html')
+    programs = Program.objects.all()
+    c = RequestContext(request, {
+        'programs': programs,
+        })
+    return HttpResponse(t.render(c))
 
 
 def program(request, program_id):
-	t = loader.get_template('program.html')
-	program = Program.objects.get(id__exact=program_id)
-	c = RequestContext(request, {
-		'program': program,
-		})
-	return HttpResponse(t.render(c))
+    t = loader.get_template('program.html')
+    program = Program.objects.get(id__exact=program_id)
+    c = RequestContext(request, {
+        'program': program,
+        })
+    return HttpResponse(t.render(c))
 
